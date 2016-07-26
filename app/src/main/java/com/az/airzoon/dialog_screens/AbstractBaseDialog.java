8df33907551cd9292efd839d7e94b9592c18909a
Siddharth yadav <siddharthdv80@gmail.com>
2016-07-27 01:18:17 +0530

    added anim

--- app/src/main/java/com/az/airzoon/dialog_screens/AbstractBaseDialog.java
+++ app/src/main/java/com/az/airzoon/dialog_screens/AbstractBaseDialog.java
@@ -8,14 +8,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 
+import com.az.airzoon.R;
+
 /**
  * Created by sid on 26/07/2016.
  */
 public abstract class AbstractBaseDialog extends Dialog implements View.OnClickListener {
 
+    public static final int ANIM_TYPE_DEFAULT = -1;
+    public static final int ANIM_TYPE_LEFT_IN_RIGHT_OUT = 0;
+    public static final int ANIM_TYPE_TOP_IN_TOP_OUT = 1;
+    private int animType = ANIM_TYPE_DEFAULT;
+
     protected Activity activity;
     private View view = null;
     private LayoutInflater layoutInflater;
+
 
     public AbstractBaseDialog(Context context) {
         super(context);
@@ -44,8 +52,30 @@ public abstract class AbstractBaseDialog extends Dialog implements View.OnClickL
 
     public abstract void onClickEvent(View actionView);
 
+
+    public void showDial0g(int animType) {
+        this.animType = animType;
+        show();
+    }
+
+    @Override
+    public void show() {
+        switch (animType) {
+            case ANIM_TYPE_LEFT_IN_RIGHT_OUT:
+                getWindow().getAttributes().windowAnimations = R.style.dialog_animation_left_in_right_out;
+                break;
+
+            case ANIM_TYPE_TOP_IN_TOP_OUT:
+                getWindow().getAttributes().windowAnimations = R.style.dialog_animation_top_in_top_out;
+                break;
+        }
+
+        super.show();
+    }
+
     @Override
     public void onClick(View view) {
         onClickEvent(view);
     }
+
 }
